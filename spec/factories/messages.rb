23d2cf71122c0aac58485993aec9408d5cde2c00
Toru Oshima <selfmadetool0213@icloud.com>
2019-06-29 00:43:46 +0900

    Create testing-framework function

---
+++ spec/factories/messages.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :message do
+    content {Faker::Lorem.sentence}
+    image {File.open("#{Rails.root}/spec/fixtures/image.jpg")}
+    user
+    group
+  end
+
+end
