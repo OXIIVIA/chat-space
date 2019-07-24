aa7640d3579b86665aec0160b3ba3199d5870fdd
Toru Oshima <selfmadetool0213@icloud.com>
2019-07-24 21:16:40 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,13 @@
 $(function() {
+  function buildHTML(data)  {
+    var html = `<div class="main-content__group">
+                  <p class="main-content__group--member">${data.user_name}</p>
+                  <p class="main-content__group--date">${data.date}</p>
+                </div>
+                <p class="main-content__text">${data.content}</p>`
+    return html;
+  }
+
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +19,15 @@ $(function() {
       dataType: "json",
       processData: false,
       contentType: false
+    })
+    .done(function(data) {
+      var html = buildHTML(data)
+      $('.main-content').append(html)
+      $('.form__box--message').val('')
+    })
+    .fail(function() {
+
+
     })
   });
 });
