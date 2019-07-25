2ad990d1f95f1562110aab802ff963b485ab41ca
Toru Oshima <selfmadetool0213@icloud.com>
2019-07-25 21:54:05 +0900

    Create done and fail method in Ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,11 +1,43 @@
 $(function() {
-  function buildHTML(data)  {
-    var html = `<div class="main-content__group">
+
+  function buildGroup(data)  {
+    var group = `<div class="main-content__group">
                   <p class="main-content__group--member">${data.user_name}</p>
                   <p class="main-content__group--date">${data.date}</p>
-                </div>
-                <p class="main-content__text">${data.content}</p>`
-    return html;
+                </div>`
+    return group;
+  }
+
+  function buildContent(data) {
+    var content =  `<div class="main-content__message">
+                      <p class="main-content__message__content">${data.content}</p>
+                    </div>`
+    return content;
+  }
+
+  function buildImage(data) {
+    var image =  `<div class="main-content__message">
+                    <img class="main-content____message__image" src="${data.image}">
+                  </div>`
+    return image;
+  }
+  function buildContentWithImage(data) {
+    var controlWithImage =  `<div class="main-content__message">
+                  <p class="main-content__message__content">${data.content}</p>
+                  <img class="main-content____message__image" src="${data.image}">
+                </div>`
+    return controlWithImage;
+  }
+
+  function getHeight() {
+    var element = document.getElementById("main-content")
+    var height = element.scrollHeight;
+    return height;
+  }
+
+  function clearImage() {
+    var element = document.getElementById("message_image")
+    element.value = "";
   }
 
   $('#new_message').on('submit', function(e) {
@@ -21,13 +53,30 @@ $(function() {
       contentType: false
     })
     .done(function(data) {
-      var html = buildHTML(data)
-      $('.main-content').append(html)
-      $('.form__box--message').val('')
+      var group = buildGroup(data);
+      var content = buildContent(data);
+      var image = buildImage(data);
+      var controlWithImage = buildContentWithImage(data);
+
+      $('.main-content').append(group);
+      if (data.content && data.image ) {
+        $('.main-content').append(controlWithImage);
+      } else if (data.content) {
+        $('.main-content').append(content);
+      } else {
+        $('.main-content').append(image);
+      }
+
+      var height = getHeight();
+
+      $('.main-content').animate({scrollTop: height}, 0);
+      $('.form__box--message').val('');
+      clearImage();
+      $('.form__submit').prop('disabled', false);
     })
     .fail(function() {
-
-
+      alert('メッセージを入力してください');
+      $('.form__submit').prop('disabled', false);
     })
   });
 });
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,6 @@
 json.content @message.content
-json.image @message.image
+json.image @message.image.url
 json.group_id @message.group_id
 json.user_id @message.user_id
+json.date @message.created_at.strftime("%Y/%m/%d %H:%M")
+json.user_name @message.user.name
